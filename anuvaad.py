3f63b0ddf659df175474e57d35e9a29290deb014
FarahSayyed <farahsmca@gmail.com>
2025-08-21 09:51:43 +0530

    bhasha bridge

---
+++ anuvaad.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+class BhashiniPipeline:
+    def __init__(self, api_key, user_id, auth_token, endpoint):
+        self.headers = {
+            "ulcaApiKey": api_key,
+            "userID": user_id,
+            "Authorization": auth_token,
+            "Content-Type": "application/json"
+        }
+        self.endpoint = endpoint
+
+    def perform_translation(self, source_text, source_lang, target_lang, service_id):
+        payload = {
+            "pipelineTasks": [
+                {
+                    "taskType": "translation",
+                    "config": {
+                        "language": {
+                            "sourceLanguage": source_lang,
+                            "targetLanguage": target_lang
+                        },
+                        "serviceId": service_id
+                    }
+                }
+            ],
+            "inputData": {
+                "input": [{"source": source_text}],
+                "audio": [{"audioContent": None}]
+            }
+        }
+        
+        response = requests.post(self.endpoint, headers=self.headers, data=json.dumps(payload))
+        response_json = response.json()
+        if response.status_code == 200:
+            translation_output = response_json.get("pipelineResponse", [{}])[0].get("output", [{}])[0].get("target", "")
+            return translation_output
+        else:
+            raise Exception(f"Translation API call failed: {response_json}")
+
+    def run_pipeline(self, source_text, source_lang, target_lang, translate=False):
+        translation_output = None
+        audio_content = None
+
+        # Translate if requested
+        if translate:
+            translation_output = self.perform_translation(
+                source_text,
+                source_lang=source_lang,
+                target_lang=target_lang,
+                service_id="ai4bharat/indictrans-v2-all-gpu--t4"
+            )
+        return {
+                "translation": translation_output
+                  }
+
+
+# usage example
+if __name__ == "__main__":
+    # initialize with your credentials and endpoint
+    pipeline = BhashiniPipeline(
+        api_key=os.getenv('ulca_api_key'),
+        user_id=os.getenv("ulca_userid"),
+        auth_token=os.getenv('authorization_key'),
+        endpoint="https://dhruva-api.bhashini.gov.in/services/inference/pipeline"
+    )
+    source_text = "This is an example of english to sanskrit translation"
+    result = pipeline.run_pipeline(source_text, "en", "sa", translate=True)
+    print(result["translation"])
---
+++ bhashabridge.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from dotenv import load_dotenv
+from anuvaad import BhashiniPipeline 
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+# set page configuration
+st.set_page_config(
+    page_title='Bhasha Bridge'
+)
+
+# initialize the BhashiniPipeline
+pipeline = BhashiniPipeline(
+    api_key=os.getenv('ulca_api_key'),
+    user_id=os.getenv("ulca_userid"),
+    auth_token=os.getenv('authorization_key'),
+    endpoint="https://dhruva-api.bhashini.gov.in/services/inference/pipeline"
+)
+
+# define language options and their corresponding codes
+language_mapping = {
+    'English': 'en',
+    'Assamese': 'as',
+    'Awadhi': 'awa',
+    'Bengali': 'bn',
+    'Bhojpuri': 'bho',
+    'Bodo': 'brx',
+    'Dogri': 'doi',
+    'Gujarati': 'gu',
+    'Hindi': 'hi',
+    'Kannada': 'kn',
+    'Kashmiri': 'ks',
+    'Khasi': 'kha',
+    'Maithili': 'mai',
+    'Malayalam': 'ml',
+    'Manipuri': 'mni',
+    'Marathi': 'mr',
+    'Mizo': 'lus',
+    'Odia': 'or',
+    'Sanskrit': 'sa',
+    'Sindhi': 'sd',
+    'Tamil': 'ta',
+    'Telugu': 'te',
+    'Urdu': 'ur'
+}
+
+# create tuples for source and target languages
+source_languages = tuple(language_mapping.keys())
+target_languages = tuple(language_mapping.keys())
+
+# title
+st.markdown('<h1 style="text-align: center;"> BhashaBridge </h1>', unsafe_allow_html=True)
+st.markdown('<h5 style="text-align: center; margin-top:-20px;"> Indian language translator </h5>', unsafe_allow_html=True)
+st.write("")
+
+# dropdowns for selecting source and target languages
+source_lang = st.selectbox("Select a source language", source_languages)
+st.write("")
+default_target_index = target_languages.index("Hindi")
+target_lang = st.selectbox("Select a target language", target_languages, index=default_target_index)
+st.write("")
+
+# get the corresponding language codes
+s_lang = language_mapping.get(source_lang)
+t_lang = language_mapping.get(target_lang)
+
+# input text box for the text to translate
+source_text = st.text_input("Enter the text to translate")
+
+# translate button
+# if st.button("Translate"):
+if source_text and source_lang and target_lang:
+    try:
+        # perform translation
+        result = pipeline.run_pipeline(
+            source_text=source_text,
+            source_lang=s_lang,
+            target_lang=t_lang,
+            translate=True
+        )
+        # display the translated text
+        st.markdown(result['translation'])
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
